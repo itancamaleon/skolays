42dcc322f2e573d056b29dfd3ab971d108ce28cc
santiago <papeleriamolly1@gmail.com>
2025-06-25 00:39:25 -0500

    V11-publicar

--- run.py
+++ run.py
@@ -3,5 +3,6 @@ from app import create_app, socketio
 app = create_app()
 
 if __name__ == '__main__':
-    app.debug = True
-    socketio.run(app, host='192.168.80.22', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
